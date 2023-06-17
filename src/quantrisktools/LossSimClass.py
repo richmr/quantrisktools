34ea64b6000e7d3cd53919f2a1fcfdd5b9227c2a
richmr <richmr@users.noreply.github.com>
2023-06-16 17:00:57 -0700

    Set up for pip installs (-e only currently)

---
+++ src/quantrisktools/LossSimClass.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 10:36:46 2017
+
+@author: MRICH
+"""
+
+from quantrisktools import LossEventClassv2
+from quantrisktools import LossCurvePlot
+from numpy import savetxt, linspace, array, sort, save, load, count_nonzero, gradient
+from scipy.integrate import simps
+import tqdm
+import pandas as pd
+
+# global replot
+
+class LossSim:
+    """
+    Performs a basic Monte Carlo simulation of attached loss events
+    """
+    
+    def __init__(self):
+        self.LossEvents = []
+        self.data = []
+        self.labels = []
+        self.minImpacts = []
+        self.plotCurves = LossCurvePlot.plotCurves
+        # Empyt dataframe as placeholder
+        self.totalLossDataframe = pd.DataFrame()
+        
+
+    def attachEvent(self, newEvent):
+        """
+        Expects events that are at LossEventClass or a subclass
+        """
+        self.LossEvents.append(newEvent)
+        #self.minImpacts.append(newEvent.minImpact)
+        
+    def run(self, iterations = 10000, aggregate = True, plot = True, presentation = False):
+        """
+        Runs all the events
+        
+        iterations = number of simulations to run
+        aggregrate = calculate aggregrate losses and include in results
+        plot = draw a basic plot automatically
+        
+        data can be retrieved via getResults method or saved via saveResults
+        """
+        
+        print("[-] LossSim.run: Beginning simulation of {} events with {} iterations each".format(len(self.LossEvents), iterations))
+        self.data = []
+        self.labels = []
+        eventnum = 0
+        totalsims = len(self.LossEvents)*iterations # number of sim iterations
+        # clear all the attached events first
+        progBar = tqdm.tqdm(total=totalsims, desc="Simulation Progress")
+        
+        for event in self.LossEvents: event.reset()
+        
+        for event in self.LossEvents:
+            thisdata=[]
+            eventnum += 1
+            for i in range(iterations):
+                thisdata.append(event.run())
+                progBar.update()
+            # self.minImpacts.append(min(thisdata))
+            # Can't do the above line because the min of every case is always zero.  We need the effective "useful" min
+            # Try getting the mean and stddev, then find LEC for stddev-3*mean
+            # for the record, stddev of a log-normal is kind of useless
+            # Hack to get a good minimal plot point from the data:
+            # Sort the data array
+            # Get the gradient of this array
+            # Count the non-zeros
+            # look for first non-zero value towards end of array from -nonzero count
+            # set minimum value to the value at sorted[-that location]
+            # No Bothans were harmed to make this hack
+            s_data = sort(thisdata)
+            gs_data = gradient(s_data)
+            index = count_nonzero(gs_data)
+            while (s_data[-index] == 0):
+                index -= 1
+            # print "[d] LossSim.run: Min impact = {}".format(s_data[-index])
+            self.minImpacts.append(s_data[-index])
+            
+            self.data.append(thisdata)
+            self.labels.append(event.EventName)
+            #newdesc = "Completed simulation of \"{}\" (Event {} of {})".format(event.EventName, eventnum, len(self.LossEvents))  
+            #progBar.set_description(desc=newdesc, refresh=True)
+            
+        progBar.close()
+        if aggregate:
+            self.labels.append("Total risk")
+            self.data.append([sum(i) for i in zip(*self.data)])
+            # Populate the data frame
+            # https://stackoverflow.com/questions/25577352/plotting-cdf-of-a-pandas-series-in-python
+            # Define your series
+            s = pd.Series(self.data[-1], name = 'loss')
+            df = pd.DataFrame(s)             
+            # Get the frequency, PDF and CDF for each value in the series             
+            # Frequency
+            stats_df = df \
+                        .groupby('loss')['loss'] \
+                        .agg('count') \
+                        .pipe(pd.DataFrame) \
+                        .rename(columns = {'loss': 'frequency'})
+             
+            # PDF
+            stats_df['pdf'] = stats_df['frequency'] / sum(stats_df['frequency'])
+             
+            # CDF
+            stats_df['cdf'] = stats_df['pdf'].cumsum()
+            stats_df = stats_df.reset_index()
+            self.totalLossDataframe = stats_df
+            
+        if plot:
+            minplot = min(self.minImpacts)
+            LossCurvePlot.plotCurves(self.data, self.labels, presentation = presentation, minimum_plotted_impact = minplot)
+                        
+            print("[-] Use 'replot(minImpact, [presentation=True/False])' if you don't like the x range of the current plot.")
+        
+    def plot(self, presentation=False):
+        """
+        Plots the data in the system
+        
+        Just a convenience method to allow plotting without running the sim again
+        """
+        LossCurvePlot.plotCurves(self.data, self.labels, presentation = presentation, minimum_plotted_impact = min(self.minImpacts))
+        return
+    
+    def replot(self, minImpact, presentation=False):
+        self.minImpacts = [minImpact]
+        self.plot(presentation)
+            
+    def getResults(self):
+        return self.data, self.labels
+    
+    def saveResults(self, filename):
+        # saves the data in binary "npy" format
+        to_save = array([self.data, self.labels])
+        save(filename, to_save)
+        print("[-] LossSim.saveResults: Simulation data saved to {}".format(filename))
+       
+        
+    def loadResults(self, filename):
+        # loads sim data
+        if (filename[-4:] != '.npy'): filename += ".npy"
+        loaded_arr = load(filename)
+        self.data = loaded_arr[0]
+        self.labels = loaded_arr[1]
+        print("[-] LossSim.loadResults: Simulation data loaded from {}".format(filename))
+        
+    def calcEAL(self, tocalc = [], method=1, steps=5000):
+        """
+        This reduces lec curves to a single value for ordinal ranking
+        
+        Returns the results in the same order they were presented
+        
+        The current method used is to integrate the area under the curve
+        TODO: I probably should make possible methods a enum structure, but skipping for now
+        
+        if an array is passed into tocalc, then that data is calculated and the results returned.
+        if not, it uses the data found in self.data.  In general, don't call until after "run"
+        """
+        if (len(tocalc) == 0): tocalc = self.data
+        results = []
+
+        print("[-] Calculating average event loss on {} events".format(len(tocalc)))        
+        for i in range(len(tocalc)):
+            data_arr = array(tocalc[i])               
+            if (method == 1):
+                # Will integrate the data under the curve
+                p = linspace(0, max(data_arr), steps)
+                lec = LossCurvePlot.calcLossExceedance(data_arr, p, forplot = False)
+                thisresult = simps(lec, p)
+                results.append(thisresult)
+                
+        return results
+    
+    def getMinAndMaxTotalLoss(self):
+        if self.totalLossDataframe.size == 0:
+            raise Exception("Can't use dataframe analysis unless run is called with aggregate=True")
+        # Ignores 0
+        minLoss = self.totalLossDataframe["loss"][1]
+        maxLoss = self.totalLossDataframe["loss"].max()
+        return minLoss, maxLoss
+    
+    def getProbabilityOfLossGreaterOrLessThan(self, lossValue):
+        if self.totalLossDataframe.size == 0:
+            raise Exception("Can't use dataframe analysis unless run is called with aggregate=True")
+        cdf_val = self.totalLossDataframe[self.totalLossDataframe["loss"] >= lossValue]["cdf"].min()
+        return 1.0-cdf_val
+        
+    def rankRisks(self, method=1, steps=5000):
+        """
+        This will return the risks in ranked order, least to most
+        
+        Uses the results and labels that result from a run
+        """
+        eal = self.calcEAL(self.data, method=method, steps=steps)
+        
+        dtype = [('label', 'S25'), ('aal', float)]
+        results = list(zip(self.labels, eal))
+        a_results = array(results, dtype)
+        a_results = sort(a_results, order='aal')
+        return a_results
+        
+        
+        
+        
---
+++ src/quantrisktools/ModelFit.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 16 15:44:19 2018
+
+@author: tmthydvnprt
+This function is adapted from the discussion at:
+https://stackoverflow.com/questions/6620471/fitting-empirical-distribution-to-theoretical-ones-with-scipy-python
+
+Though I've made it easy to use, I did NOT write this awesome code - mrich
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#plt.hist(finesdata)
+#%matplotlib inline
+import warnings
+import pandas as pd
+import scipy.stats as st
+import matplotlib
+
+
+matplotlib.rcParams['figure.figsize'] = (16.0, 12.0)
+matplotlib.style.use('ggplot')
+
+# Create models from data
+def best_fit_distribution(data, bins=200, ax=None):
+    """Model data by finding best fit distribution to data"""
+    # Get histogram of original data
+    y, x = np.histogram(data, bins=bins, density=True)
+    x = (x + np.roll(x, -1))[:-1] / 2.0
+
+    # Distributions to check
+    DISTRIBUTIONS = [        
+        st.alpha,st.anglit,st.arcsine,st.beta,st.betaprime,st.bradford,st.burr,st.cauchy,st.chi,st.chi2,st.cosine,
+        st.dgamma,st.dweibull,st.erlang,st.expon,st.exponnorm,st.exponweib,st.exponpow,st.f,st.fatiguelife,st.fisk,
+        st.foldcauchy,st.foldnorm,st.genlogistic,st.genpareto,st.gennorm,st.genexpon,
+        st.genextreme,st.gausshyper,st.gamma,st.gengamma,st.genhalflogistic,st.gilbrat,st.gompertz,st.gumbel_r,
+        st.gumbel_l,st.halfcauchy,st.halflogistic,st.halfnorm,st.halfgennorm,st.hypsecant,st.invgamma,st.invgauss,
+        st.invweibull,st.johnsonsb,st.johnsonsu,st.ksone,st.kstwobign,st.laplace,st.levy,st.levy_l,st.levy_stable,
+        st.logistic,st.loggamma,st.loglaplace,st.lognorm,st.lomax,st.maxwell,st.mielke,st.nakagami,st.ncx2,st.ncf,
+        st.nct,st.norm,st.pareto,st.pearson3,st.powerlaw,st.powerlognorm,st.powernorm,st.rdist,st.reciprocal,
+        st.rayleigh,st.rice,st.recipinvgauss,st.semicircular,st.t,st.triang,st.truncexpon,st.truncnorm,st.tukeylambda,
+        st.uniform,st.vonmises,st.vonmises_line,st.wald,st.weibull_min,st.weibull_max,st.wrapcauchy,
+        # st.frechet_r,st.frechet_l,
+    ]
+
+    # Best holders
+    best_distribution = st.norm
+    best_params = (0.0, 1.0)
+    best_sse = np.inf
+
+    # Estimate distribution parameters from data
+    for distribution in DISTRIBUTIONS:
+
+        # Try to fit the distribution
+        try:
+            # Ignore warnings from data that can't be fit
+            with warnings.catch_warnings():
+                warnings.filterwarnings('ignore')
+
+                # fit dist to data
+                params = distribution.fit(data)
+
+                # Separate parts of parameters
+                arg = params[:-2]
+                loc = params[-2]
+                scale = params[-1]
+
+                # Calculate fitted PDF and error with fit in distribution
+                pdf = distribution.pdf(x, loc=loc, scale=scale, *arg)
+                sse = np.sum(np.power(y - pdf, 2.0))
+
+                # if axis pass in add to plot
+                try:
+                    if ax:
+                        pd.Series(pdf, x).plot(ax=ax)
+                    
+                except Exception:
+                    pass
+
+                # identify if this distribution is better
+                if best_sse > sse > 0:
+                    best_distribution = distribution
+                    best_params = params
+                    best_sse = sse
+
+        except Exception:
+            pass
+
+    return (best_distribution.name, best_params)
+
+def make_pdf(dist, params, size=10000):
+    """Generate distributions's Propbability Distribution Function """
+
+    # Separate parts of parameters
+    arg = params[:-2]
+    loc = params[-2]
+    scale = params[-1]
+    print(f"{arg}, {loc}, {scale}")
+
+    # Get sane start and end points of distribution
+    start = dist.ppf(0.01, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.01, loc=loc, scale=scale)
+    print(f"{start}")
+    end = dist.ppf(0.99, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.99, loc=loc, scale=scale)
+    print(f"{end}")
+
+    # Build PDF and turn into pandas Series
+    x = np.linspace(start, end, size)
+    print(f"{x[:3]}")
+    y = dist.pdf(x, *arg, loc=loc, scale=scale)
+    pdf = pd.Series(y*size, x)
+
+    return pdf
+
+def distro_hist(dist, params, trials, bins=200):
+    trial_results = dist(*params).rvs(trials)
+    y, x = np.histogram(trial_results, bins=bins)
+    return y,x, trial_results
+   
+    
+
+def do_fit(data, chartTitle = "Data", xlabel = '$'):
+    """
+    data = array of data
+    chartTitle = Top line to be displayed on the generated charts
+    xlabel = Label for x axis
+    """
+ 
+    data = pd.Series(data)
+    
+       
+    # Find best fit distribution
+    best_fit_name, best_fir_paramms = best_fit_distribution(data, 200)
+    best_dist = getattr(st, best_fit_name)
+    
+    
+    # "giggle" check the fit
+    trial_results = pd.Series(best_dist(*best_fir_paramms).rvs(len(data)))
+    
+    # Display
+    plt.figure(figsize=(12,8))
+    ax = data.plot(kind='hist', bins=50, alpha=0.5, label='Data', legend=True) # normed=True, deprecated?
+    trial_results.plot(kind='hist', bins=50, alpha=0.5, label="Model Trials", legend=True, ax=ax)
+    
+    param_names = (best_dist.shapes + ', loc, scale').split(', ') if best_dist.shapes else ['loc', 'scale']
+    param_str = ', '.join(['{}={:0.2f}'.format(k,v) for k,v in zip(param_names, best_fir_paramms)])
+    dist_str = '{}({})'.format(best_fit_name, param_str)
+    
+    ax.set_title(chartTitle + '\nBest fit distribution \n' + dist_str)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel('Frequency')
+    
+    # Print the dist string for easy cut and paste
+    print("Best fit: scipy.stats.{}".format(dist_str))
+
+# Experiments
+def test1():
+    dist = st.johnsonsb(a=1.60, b=0.49, loc=7474.12, scale=17452673.57)
+    dist_plain = st.johnsonsb
+    params = [1.60, 0.49, 474.12, 17452673.57]
+    pdf = make_pdf(dist_plain, params, size=int(1e6))
+    return dist, pdf
+
+def test2():
+    dist = st.johnsonsb(a=1.60, b=0.49, loc=7474.12, scale=17452673.57)
+    dist_plain = st.johnsonsb
+    params = [1.60, 0.49, 474.12, 17452673.57]
+    y,x,tr = distro_hist(dist_plain, params, 64)
+    return x,y,tr
+    
+    
+#dist, pdf = test1()
+#x,y,tr = test2()
+#pdf.plot()
+    
